8e59093085928b0e9b00ad6f9bddda41fcf35291
Jur <jurrijs04@gmail.com>
2023-09-08 09:16:58 +0200

    empty projects

---
+++ Individual Project/WebApp1/Dtos/Mapper.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp1.Dtos
+{
+	public class Mapper
+	{
+		/*
+		 * mapt entitie classes naar dto classes en andersom
+		 * 
+		 * let op, zorg dat niet te veel database calls worden gemaakt. dit maakt het langzaam
+		 */
+	}
+}
