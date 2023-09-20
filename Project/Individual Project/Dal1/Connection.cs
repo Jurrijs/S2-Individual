f65814cd110da0677e5cf4d08e7730e56f77e08d
Jur <jurrijs04@gmail.com>
2023-09-20 09:33:57 +0200

    empty project

---
+++ Project/Individual Project/Dal1/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal1
+{
+	public class Connection
+	{
+		private string _connectionString = "";
+
+		public string ConnectionString { get { return _connectionString; } }
+	}
+}
---
+++ Project/Individual Project/WebApp1/Dtos/Dtos/TestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp1.Dtos.Dtos
+{
+    public class TestDto
+    {
+    }
+}
